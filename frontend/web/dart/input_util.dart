d22d00787b8b4f8b8e2bd767e8e001b2c27b2943
doodlezucc <electerminator@gmail.com>
2020-12-29 19:23:01 +0100

    added number inputs after sliders

--- frontend/web/dart/input_util.dart
+++ frontend/web/dart/input_util.dart
@@ -1,19 +1,51 @@
 import 'dart:html';
 
-void onInput(InputElement input, void Function(double v) onValueChange,
+import 'dart:math' as math;
+
+void onInput(InputElement main, void Function(double v) onValueChange,
     {bool writeToSibling = true, int digits = 0}) {
-  void apply() {
-    var v = input.valueAsNumber;
+  var min = double.parse(main.min);
+  var max = double.parse(main.max);
+  var sib = main.nextElementSibling;
+
+  num validate(num v) {
+    if (v.isNaN) v = 0;
+    return math.min(max, math.max(min, v));
+  }
+
+  void apply([InputElement a, InputElement b]) {
+    var v = (a ?? main).valueAsNumber;
+    v = validate(v);
     onValueChange(v);
     if (writeToSibling) {
-      input.nextElementSibling.text = v.toStringAsFixed(digits);
+      var vString = v.toStringAsFixed(digits);
+      (b ?? sib as InputElement).value = vString;
+      if (b != null) {
+        _redraw(b, v, min, max);
+      }
     }
   }
 
-  listenApply(input.onInput, apply);
-  input.onChange.listen((_) {
-    apply();
-  });
+  if (writeToSibling && sib is InputElement) {
+    sib
+      ..value = main.value
+      ..min = main.min
+      ..max = main.max
+      ..step = main.step;
+    sib.onInput.listen((_) {
+      apply(sib, main);
+    });
+    sib.onBlur.listen((_) => sib.value = main.value);
+    sib.onKeyDown.listen((e) {
+      // Unfocus input on Esc or Enter key
+      if (e.keyCode == 13 || e.keyCode == 27) {
+        sib.blur();
+      }
+    });
+  }
+
+  listenApply(main.onInput, apply);
+  main.onChange.listen((_) => apply());
 }
 
 void listenApply(Stream stream, void Function() apply) {
